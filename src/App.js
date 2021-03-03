db28c8b1458464d84d030774c1e94f9659962693
Paul Hendrickson <PaulRobHendrickson@gmail.com>
2021-03-02 21:09:18 -0600

    a bunch commented out

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import React from "react";
-// import Main from "./components/Main";
-// import Wrapper from "./components/Wrapper";
-// import Header from "./components/Header";
+import Main from "./components/Main";
+import Wrapper from "./components/Wrapper";
+import Header from "./components/Header";
 
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      hi
-      {/* <Wrapper>
+      <Wrapper>
         <Header />
-         <Main /> 
-      </Wrapper> */}
+        {/* <Main />  */}
+      </Wrapper>
     </div>
   );
 }
